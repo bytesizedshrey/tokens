cf72731f07b287a781df00c5466413428bcb261d
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-02 10:19:54 +0530

    Refactor registerController to use registerService for user registration logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,34 +1,10 @@
 const userModel = require("../models/user.model");
 let bcrypt = require('bcrypt');
 const { generateAccessToken, generateRefreshToken } = require("../utils/generateToken");
+const registerService = require("../services/auth.service");
 
 let registerController = async (req, res) => {
-  let { name, email, password } = req.body;
-
-  if (!email || !password)
-    return res.status(400).json({
-      message: "All fields are required",
-    });
-
-    let isExisted = await userModel.findOne({
-        email,
-    })
-
-    if(isExisted)
-        return res.status(409).json({
-        message : 'User already exists with this email'        
-    })
-
-    let hashPass = bcrypt.hashSync(password,10)
-
-    let newUser = await userModel.create({
-        name,
-        email,
-        password : hashPass,
-    })
-
-    let accessToken = generateAccessToken(newUser._id);
-    let refreshToken = generateRefreshToken(newUser._id);
+  let {accessToken,refreshToken,newUser} = await registerService(req.body)
 
     res.cookie("accessToken", accessToken, {
         httpOnly : true,
---
+++ src/services/auth.service.js
@@ -0,0 +1,38 @@
+let registerService = async (data) => {
+    let { name, email, password } = data;
+    try {
+        if (!email || !password)
+            return res.status(400).json({
+              message: "All fields are required",
+            });
+        
+            let isExisted = await userModel.findOne({
+                email,
+            })
+        
+            if(isExisted)
+                return res.status(409).json({
+                message : 'User already exists with this email'        
+            })
+        
+            let hashPass = bcrypt.hashSync(password,10)
+        
+            let newUser = await userModel.create({
+                name,
+                email,
+                password : hashPass,
+            })
+        
+            let accessToken = generateAccessToken(newUser._id);
+            let refreshToken = generateRefreshToken(newUser._id);
+
+            return {
+                accessToken,refreshToken,newUser
+            }
+
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+module.exports=registerService
