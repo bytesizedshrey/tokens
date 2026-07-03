0e8fa84e48a8c51ebd6720bbdb54c71ffa2221da
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-03 16:54:34 +0530

    Use HTTPS and add Axios response interceptor
    Change axios baseURL to use HTTPS, comment out withCredentials, and add a response interceptor that logs responses and errors. On a 401 error the interceptor triggers a GET to /get-accessToken to attempt refreshing the access token. This centralizes basic response logging and a simple 401 refresh attempt.

--- tokens_frontend/src/config/axiosInstance.jsx
+++ tokens_frontend/src/config/axiosInstance.jsx
@@ -2,6 +2,21 @@ import axios from 'axios'
 
 
 export let axiosInstance = axios.create({
-    baseURL : 'http://fakestoreapi.com',
-    withCredentials: true
+    baseURL : 'https://fakestoreapi.com',
+    // withCredentials: true
 })
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        console.log("Axios instance response -> ", response)
+        return response
+    },
+    (error) => {
+        console.log('error in instance : ', error)
+
+
+        if(error.response.status === 401){
+            axiosInstance.get('/get-accessToken')
+        }
+    }
+)
